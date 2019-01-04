56d129570d31b7d10276ac169a12538687adf764
daniel <danielbinyamin@gmail.com>
2019-01-04 02:24:38 +0200

    finished implementing SIgnedHackthonPage and UnsignedHackathonPage

--- app/src/main/java/com/appckathon/appckathon/ManagedHackathonPage.java
+++ app/src/main/java/com/appckathon/appckathon/ManagedHackathonPage.java
@@ -4,17 +4,35 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class ManagedHackathonPage extends AppCompatActivity {
 
+    //TODO: need to pass hackathon object from previouse page
+    private Hackathon hackthon = (Hackathon) getIntent().getSerializableExtra("hackathon");
+
+    private TextView hackathonName;
+    private TextView managerName;
+    private TextView description;
+    private ListView teams_list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_managed_hackathon_page);
-        //TODO: insert actual list of teams
-        String[] teams_arr = {"team1", "team2", "team3", "team4"};
-        ListView teams_list = (ListView)findViewById(R.id.hackathon_teams_list);
+        hackathonName = (TextView) findViewById(R.id.hackathon_name);
+        managerName = (TextView) findViewById(R.id.hackathon_manager_name);
+        description = (TextView) findViewById(R.id.hackathon_description);
+
+        //set text for these TextViews
+        hackathonName.setText(hackthon.getName());
+        managerName.setText(hackthon.getManagerName());
+        description.setText(hackthon.getDescription());
+
+        String[] teams_arr = (String[]) hackthon.getTeams().toArray();
+        teams_list = (ListView)findViewById(R.id.hackathon_teams_list);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, teams_arr);
         teams_list.setAdapter(adapter);
+
+        //TODO:(daniel) handle editable fields and change them in FB
     }
 }
--- app/src/main/java/com/appckathon/appckathon/UnsignedHackathonPage.java
+++ app/src/main/java/com/appckathon/appckathon/UnsignedHackathonPage.java
@@ -1,21 +1,60 @@
 package com.appckathon.appckathon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class UnsignedHackathonPage extends AppCompatActivity {
+
+    //TODO: need to pass hackathon object from previouse page
+    private Hackathon hackthon = (Hackathon) getIntent().getSerializableExtra("hackathon");
+
+    private TextView hackathonName;
+    private TextView managerName;
+    private TextView description;
+    private ListView teams_list;
+    private Button joinHackathon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_unsigned_hackathon_page);
-        //TODO: insert actual list of teams
-        String[] teams_arr = {"team1", "team2", "team3", "team4"};
-        ListView teams_list = (ListView)findViewById(R.id.hackathon_teams_list);
+        setContentView(R.layout.activity_managed_hackathon_page);
+        hackathonName = (TextView) findViewById(R.id.hackathon_name);
+        managerName = (TextView) findViewById(R.id.hackathon_manager_name);
+        description = (TextView) findViewById(R.id.hackathon_description);
+
+        //set text for these TextViews
+        hackathonName.setText(hackthon.getName());
+        managerName.setText(hackthon.getManagerName());
+        description.setText(hackthon.getDescription());
+
+        String[] teams_arr = (String[]) hackthon.getTeams().toArray();
+        teams_list = (ListView) findViewById(R.id.hackathon_teams_list);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, teams_arr);
         teams_list.setAdapter(adapter);
+
+        joinHackathon = (Button) findViewById(R.id.join_hackathon);
+        joinHackathon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                joinHackathon();
+            }
+        });
+
+    }
+
+    private void joinHackathon() {
+        //TODO: (tal) add "hackthhon" object to current user hackathon list(participating list)
+
+        //once succesfully added user toast + move to home page
+        Toast.makeText(UnsignedHackathonPage.this, "joined hackathon successfully! ", Toast.LENGTH_SHORT).show();
+        //navigate back to home page
+        startActivity(new Intent(UnsignedHackathonPage.this, HomePage.class));
     }
 }
